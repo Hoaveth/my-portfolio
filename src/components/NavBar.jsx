a6a11bcb8f28a49c5fe1b9308ea717886cc43e29
Hoaveth <hoaveth.gaming@gmail.com>
2023-04-16 21:32:35 +0800

    Completed the hamburger menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -43,13 +43,23 @@ const NavBar = () => {
       </ul>
       <div
         onClick={() => setNav(!nav)}
-        className="cursor-pointer pr-4 z-10 text-gray-500"
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
       >
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
-      <ul className="cursor-pointer flex flex-col justify-center items-center">
-        <li>Test</li>
-      </ul>
+
+      {nav && (
+        <ul className="cursor-pointer flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 py-6 cursor-pointer text-4xl capitalize font-medium text-gray-500 hover:text-white hover:scale-105 duration-200"
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
