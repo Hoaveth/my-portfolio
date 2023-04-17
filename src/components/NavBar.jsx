0b1d9629a9f4ddba559e7eacf08f1ddf546740f5
Hoaveth <hoaveth.gaming@gmail.com>
2023-04-17 12:34:34 +0800

    Fixed navbar and added links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import NavLinks from "./NavLinks";
 
 const NavBar = () => {
   const [nav, setNav] = useState(false);
+
   const links = [
     {
       id: 1,
@@ -27,36 +29,37 @@ const NavBar = () => {
   ];
 
   return (
-    <div className="flex justify-between items-center w-full h-20  text-white fixed px-4 bg-black ">
+    <div className="flex justify-between items-center w-full h-20 px-4 bg-black text-white fixed">
       <div>
-        <h1 className="font-signature text-5xl  ml-2">Hoaveth</h1>
+        <h1 className="text-5xl font-signature ml-2">Hoaveth</h1>
       </div>
+
       <ul className="hidden md:flex">
         {links.map(({ id, link }) => (
-          <li
+          <NavLinks
             key={id}
-            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:text-white hover:scale-105 duration-200"
-          >
-            {link}
-          </li>
+            link={link}
+            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-white duration-200"
+          />
         ))}
       </ul>
+
       <div
         onClick={() => setNav(!nav)}
-        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+        className="cursor-pointer pr-4 md:hidden z-10 text-gray-500"
       >
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
 
       {nav && (
-        <ul className="cursor-pointer flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
           {links.map(({ id, link }) => (
-            <li
+            <NavLinks
               key={id}
-              className="px-4 py-6 cursor-pointer text-4xl capitalize font-medium text-gray-500 hover:text-white hover:scale-105 duration-200"
-            >
-              {link}
-            </li>
+              link={link}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl hover:scale-105 duration-200"
+              onClick={() => setNav(!nav)}
+            />
           ))}
         </ul>
       )}
